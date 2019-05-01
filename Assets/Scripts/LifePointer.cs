e15a5d8f6dfbb28243767abda1891947c686df9a
ViacheslavMaksymov <Maximov.Vyacheslav@outlook.com>
2019-05-01 19:26:55 +0300

    Revert "commit2"
    This reverts commit 3acecc876855eaaf3159b74869335598de5bfe46.

---
+++ Assets/Scripts/LifePointer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class LifePointer : MonoBehaviour { //Class for player life cycle management
+
+    public Text textMessage, textLife;  
+    static int LifePoint = 10;
+
+	void Start () {
+    }
+ 
+    public static void SubLifePoint()
+    {
+        LifePoint -= 1;
+    }
+
+    public static void addLifePoint(int count)
+    {
+        LifePoint = count;
+    }
+
+    public static void EndGame()
+    {
+        Score.SetHighScore();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if(LifePoint <= 0)
+        {
+            EndGame();
+            SceneManager.LoadScene("EndGame");
+        }
+
+        textLife.text = LifePoint.ToString();
+
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovment.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour {
+
+	public float BaseMoveSpeed = 8f;
+	private float ShipRaius = 0.24f;
+    private float BorderPlayer;
+    float withOrtho;
+    private float screenRatio = (float)Screen.width / (float)Screen.height;
+
+	// Use this for initialization
+	void Start () {
+        BorderPlayer = Camera.main.orthographicSize  * 0.5f;
+        withOrtho = Camera.main.orthographicSize * screenRatio;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		Vector2 pos = transform.position;
+		
+		pos.x += BaseMoveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime;
+        pos.y += BaseMoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+
+        if (pos.x + ShipRaius > withOrtho) {
+			pos.x = withOrtho - ShipRaius;
+		}
+
+		if (pos.x - ShipRaius < -withOrtho) {
+			pos.x = -withOrtho + ShipRaius;
+		}
+
+
+        if (pos.y + ShipRaius > BorderPlayer - Camera.main.orthographicSize)
+        {
+            pos.y = BorderPlayer - ShipRaius - Camera.main.orthographicSize;
+        }
+
+        if (pos.y - ShipRaius < -Camera.main.orthographicSize)
+        {
+            pos.y = -Camera.main.orthographicSize + ShipRaius;
+        }
+
+        transform.position = pos;
+
+
+	}
+}
